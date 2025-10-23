29b017f1ade6f3dcba18c5062de141a7855c5fed
Pritam Kumari <pritamchaudhary96@gmail.com>
2025-10-24 01:00:36 +0530

    First Commit

---
+++ android/app/src/main/java/com/SharpBrainer/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.SharpBrainer.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.SharpBrainer.app',
+  appName: 'Sharp Brainer',
+  webDir: 'dist/Sharp-Brainer/browser'
+};
+
+export default config;
